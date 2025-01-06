2235ba8d5f09dc565c0bfa4dc0948be96989ba35
Daniel Orimolade <Danieljigga@outlook.com>
2025-01-06 16:32:05 -0500

    chng subnet config

--- Terraform-VPC/modules/vpc/main.tf
+++ Terraform-VPC/modules/vpc/main.tf
@@ -13,7 +13,7 @@ resource "aws_subnet" "subnets" {
   count = length(var.subnet_cidr)
   vpc_id     = aws_vpc.my_vpc.id
   cidr_block = var.subnet_cidr[count.index]
-  availability_zone = data.aws_availability_zones.available.names[count.index]
+  availability_zone = element(data.aws_availability_zones.available.names, count.index)
   map_public_ip_on_launch = true
   
   tags = {
